78a21eb06b1d8fe53e6a1999f87b6e7529bda99a
poolsidebill <bcase59@gmail.com>
2023-03-31 09:00:07 -0500

    major rewrite to add menu options for control

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # macroPhotoShooter
 
 ## Overview
-Control Canon R5 camera wirelessly via Canon's CCAPI RESTful interface, and a 3D Printer bed serially via GCode/MCode to create multiple images for macrophotography. The printer bed acts as a slide for the subject while the camera stays still. The program determines the Depth Of Field based on user input, then controls the bed's Y-axis movement between camera captures.
+Control Canon R5 camera wirelessly via Canon's CCAPI RESTful interface, and a 3D Printer bed serially via GCode/MCode to create multiple images for macrophotography. The printer bed acts as a slide for the subject while the camera stays still.
+ The program determines the Depth Of Field based on user input, then controls the bed's Y-axis movement between camera automatic captures. All images captured can be transfered wirelessly to a local file directory. Original images are kept on camera medium.
+ Stacking of images is not performed by this program. One option is to use my [photoStacker script](https://github.com/poolsidebill/photoStacker), but plenty of other options exist.
 
 Developed with:
 |Program|Version|
@@ -23,19 +25,36 @@ Code base was written in Python 3 and tested on Linux Mint 20.3
 - **r5_cameraUtils.py** - Utilities controlling the R5 camera and image collection
 - **gcodeUtils.py** - Utilities controlling 3D Printer and bed placement
 
+## Menu Options
+
+|Number|Name|Description|
+|------|----|-----------|
+|1  |Printer Status|Check or establish 3D printer connection and control. Homes printer bed and optionally moves Z-axis out of the way of the picture area|
+|2  |Camera Status   | Check or establish camera control. Reports battery atatus to confirm  RESTful CCAPI is working  |
+|3  |Define Shot Parameters   |Define parameters of **camera** (fstop and lens focal length) and **subject** (size and distance to camera focal plane). This information is used to determine the number of images required to capture the subject at current Depth Of Field and bed movement between each shot   |
+|4 | Check Shot Endpoints   | Specify Front-to-Back or Back-to-Front shooting direction. Bed is moved between first and last shooting position (as determined in option 3) allowing user to check lighting and framing of subject  |
+|5 |Perform Shot Captures   | Automatic control of bed movement and camera to capture the number of images (defined via option 3) required. Images captured can then be transfered from camera to a local directory for further processing (i.e. stacking). Transfering of images is controlled by a prompt. Original images will always remain on the camera  |
+|6   |Print Bed Location   | Queries the printer for current X, Y, Z axis locations and displays the results  |
+|7   | Change Z-axis  | Move Z axis on printer. Prompts for direction and distance to move the Z axis. Used to manually adjust postion of Z axis. Just a feature that comes in handy when you need it  |
+|8   | Exit  |Leave this program  |
+
 ## General Info
 - All measurements are in millimeters - so much easier that way
-- Subject images will be shot starting from front and working toward the back of the subject
-- Camera should be in manual mode and prefocused on a spot on the subject **closest** to the camera
-- Prior to the start of shooting, the bed will move away from focus point to ensure the closest point of the subject is captured
-- As shooting progresses, the subject will move closer to the camera. Ensure your lighting stays consistent and shadows do not creep in unexpectedly
+- Camera should be in manual mode and prefocused on a spot on the subject
+- As a default, subject images will be shot starting from front and working toward the back of the subject
+    - Front to Back shooting - suggested normal subject size
+    - Back to Front shooting - suggested for larger subject size
+    - Shooting direction can be changed under menu option **4 - Check Shot Endpoints**
+- As shooting progresses, the subject will move closer or further from to the camera based on the shooting direction described above. Ensure your lighting stays consistent and shadows do not creep in unexpectedly
+    - Check lighting and image composition with menu option **4 - Check Shot Endpoints**
 - Canon CCAPI does not currently allow creating folders to hold these images. Start each shooting session with a new folder on the camera to hold the images captured.
 
 ## Things to Do
  (no particular order)
 - graphical frontend
 - Checks to ensure subject length isn't too large for bed movement from current starting point
+- Save shot parameters to file for macro shot history
 - Planning only option. Display various Depth of Field values and projected image counts for various F-Stop and subject lengths
-- Lighting monitor. Precheck lighting conditions between starting and ending bed postions with the subject
+- **Added 30Mar23** ~~Lighting monitor. Precheck lighting conditions between starting and ending bed postions with the subject~~
 - **Added 08Mar23** ~~Move images from camera to computer (via CCAPI) for later image stacking~~
 - Update code comments
--- macroPhotoShooter.py
+++ macroPhotoShooter.py
@@ -15,25 +15,60 @@
     to keep everythin sync'd.
 
     Note:
-    1) Bed movement is TOWARDS the front of the machine, TOWARDS the camera.
-       Camera should be pre-focused to front tip of subject.
-    2) Camera shutter setting should be mechanical or 1st Electronic.
-       Do NOT set shutter to Electronic.The time between sending the shutter
-       Press and Release commands allows multiple pictures to be taken instead
-       of only one.
+    1) Camera shutter setting should be set to single shot mode, otherwise the
+       time between sending the shutter Press and Release commands allows
+       multiple pictures to be taken instead  of only one.
 
 """
-import os
-import subprocess
-from r5_cameraUtils import *
-from gcodeUtils import *
+# import os
+# import subprocess
+import sys
+import time
+from datetime import datetime
+from r5_cameraUtils import (
+    createR5Session,
+    depthOfField,
+    stackingDOF,
+    getLastEvent,
+    shootR5Image,
+    copyFiles,
+    reportBatteryStatus,
+)
+from gcodeUtils import (
+    connect3dPrinter,
+    homePrinter,
+    setAbsPositioning,
+    setRelPositioning,
+    quickMove,
+    slowMove,
+    setOrigin,
+    gotoOrigin,
+    printBedPosition,
+    moveAxisZ,
+)
 
-def setupPrinter(prtConn=None, homePrt=True, yAxis=120):
+# Globals
+prtConn = None  # serial object used to communicate with printer
+prtReady = False  # has connection to printer been established and bed configured
+r5Session = None  # Request.Session object used to communicate with camera
+camReady = (
+    False
+)  # camera has its CCAPI endpoint enabled and initial connection established
+fStop = 0.0  # camera FStop for image capture
+focalLen = 100  # focal length of camera lens - my default macro lens is 100mm
+subjectDist = 0  # distance from subject to camera focal plane
+subjectLen = 0  # length of subject capture
+dof = 0.0  # calculated Depth of Field
+bedMoveIncrement = 0.0  # calculated Y-axis movement between image captures
+numShots = 0  # calculated number of shots required for subject capture
+shotDirection = 1 # default direction is Front to Back. -1 for Back to Front
+
+def setupPrinter(prtConn=None, homePrt=True, yAxis=110):
     """ Send 3D-printer to known location and move Z axis rail out of way
 
     Send printer to the known home of machine (this uses machine's limit switches).
     Move the bed to a known Y position that allows the bed to used as a platform
-    for the macro subject. The Z rail holding teh extruder is moved high out of
+    for the macro subject. The Z rail holding the extruder is moved high out of
     the way. Configure 3D-printer to use relative positioning so bed movement
     commands only specify how far to move on each command, not to the actual
     XYZ axis position.
@@ -43,137 +78,333 @@ def setupPrinter(prtConn=None, homePrt=True, yAxis=120):
       homePrt: send the X,Y,Z axis to mechanical home position
       yAxis: starting Y position of bed
     """
-    if homePrt: homePrinter(prtConn)
+    if homePrt:
+        homePrinter(prtConn, True) # home X,Y but leave Z axis where it is
     setAbsPositioning(prtConn)
-    quickMove(prtConn, x=0, y=yAxis, z=190) # adjust bed and move zrail out of way
-    setRelPositioning(prtConn) #91
-    setOrigin(prtConn) # sets x=0 y=0 z=staysAtCurrentValue
+    # quickMove(prtConn, x=0, y=yAxis, z=190)  # adjust bed and move zrail out of way
+    quickMove(prtConn, x=0, y=yAxis)  # adjust bed and move zrail out of way
+    setRelPositioning(prtConn)  # 91
+    setOrigin(prtConn)  # sets x=0 y=0 z=staysAtCurrentValue
+    moveZ = input("\n\t Move Z axis out of way? y or (n): ")
+    if not moveZ == "" or not  moveZ.upper() == 'N':
+        moveAxisZ(prtConn)
+
 
 def getShotParams():
     """ Inquire user about camera configuration and object placement
 
     Set the following based on user input. Prompt provided to fix mistakes before
-    returning the values collected.
+    returning the values collected. All parameters are globals, and are used as the
+    default values for each prompt.
 
-    Returns:
+    Globals updated:
       fStop - FStop setting of the camera
       focalLen - Focal length of lens (in mm)
       subjectDist - Distance leading edge of object is from the end of the camera lens
       subjectLen - Length of the subject photograph. Determines total bed movement
     """
+    global fStop, focalLen, subjectDist, subjectLen
     while True:
-        fStop = (float(input("\n\t Enter camera FStop: ")))
-        focalLen = int((input("\t Enter lens focal length: ")))
-        subjectDist = int((input("\t Enter distance from lens to subject (in mm): ")))
-        subjectLen = int((input("\t Enter length of subject (in mm): ")))
-        paramTxt = "\n\tFStop= {fStop} Lens_length = {fLen} distance_to_object = {sDist} subject_size = {sLen}"
-        print(paramTxt.format(fStop=fStop, fLen=focalLen, sDist=subjectDist, sLen=subjectLen))
+        temp = input(f'\n\t Enter camera FStop (default={fStop}) : ')
+        if not temp == "":
+            fStop = float(temp)
+        temp = input(f'\t Enter lens focal length (default={focalLen}) : ')
+        if not temp == "":
+            focalLen = int(temp)
+
+        temp = input(f'\t Enter distance from lens to subject in mm (default={subjectDist}) : ')
+        if not temp == "":
+            subjectDist = int(temp)
+
+        temp = input(f'\t Enter length of subject in mm (default={subjectLen}): ')
+        if not temp == "":
+            subjectLen = int(temp)
+
+        paramTxt = "\n\t FStop= {fStop} Lens_length = {fLen}mm distance_to_object = {sDist}mm subject_size = {sLen}mm"
+        print(
+            paramTxt.format(
+                fStop=fStop, fLen=focalLen, sDist=subjectDist, sLen=subjectLen
+            )
+        )
         response = input("\n\t Does this look right? y or n : ")
 
         if "Y" == response.upper():
+            print("\n")
             break
 
-    return fStop, focalLen, subjectDist, subjectLen
 
-def determineShotMovements( dof, objectLen):
+def determineShotMovements(dof, objectLen):
     stackingDepth = stackingDOF(dof)
-    numShots = int(subjectLen/stackingDepth) + 4 # add extra. 2 before and 2 after
-    return round(stackingDepth,1), numShots
+#    numShots = int(subjectLen / stackingDepth) + 4  # add extra. 2 before and 2 after
+    numShots = int(subjectLen / stackingDepth) + 2  # add extra. 2 after
+    return round(stackingDepth, 2), numShots
+
 
 def decodeTime(seconds):
     sec = int(seconds)
     ty_res = time.gmtime(sec)
-    return time.strftime("%H:%M:%S",ty_res)
+    return time.strftime("%H:%M:%S", ty_res)
+
 
 def printShotEstimate(bedMoveIncrement, numShots):
-    timeGuess = round(numShots * 1.1, 0) #
+    timeGuess = round(numShots * 1.1, 0)  #
     shotClockTxt = "\t Bed movement per shot = {bm}mm Number of shots = {ns}  estimated time (HH:MM:SS) = {et}"
     print("\n")
     print("++" * 50)
-    print( shotClockTxt.format(bm=bedMoveIncrement, ns=numShots, et=decodeTime(timeGuess)))
+    print(
+        shotClockTxt.format(bm=bedMoveIncrement, ns=numShots, et=decodeTime(timeGuess))
+    )
     print("++" * 50)
 
 
-# main
-print("\n Starting to connect to printer and camera...\n")
-try:
-    prtConn, success = connect3dPrinter()
-    if success:
-        setupPrinter(prtConn)
-        prtReady = True
+def printMenu():
+    menuStatusTxt = "\t Printer Connected: {prtStat} \t  Camera Connected: {camStat}\n"
+    menuOptionTxt = "\t {optNum}  --- {opt}"
+    print("\n\n\t\t Macro Photo Shooter Main Menu \n")
+    print(menuStatusTxt.format(prtStat=prtReady, camStat=camReady))
+    for key in menuOptionDict.keys():
+        print(menuOptionTxt.format(optNum=key, opt=menuOptionDict[key]))
 
-    r5Session, success = createR5Session()
-    camReady = success
 
-    while True:  # Setup for shooting until user has had enough
 
-        if prtReady and camReady:
-            # interfaces established
-            fStop, focalLen, subjectDist, subjectLen = getShotParams()
-            dof = depthOfField( dist=subjectDist, fStop=fStop,focalLen=focalLen)
-            bedMoveIncrement, numShots = determineShotMovements( dof, subjectLen)
-            printShotEstimate(bedMoveIncrement, numShots) # tell user about time info for these shots
+def checkPrinterStatus():
+    try:
+        # show if printer is connected and current X, Y,Z coordinates
+        global prtConn, prtReady
+        print("\tcheckPrinterStatus")
+        if prtConn is None:
+            resp = input("\tPrinter is not connected: Retry connection (y) or n: ")
+            if resp == "" or "Y" == resp.upper():
+                prtConn, success = connect3dPrinter()
+                if success:
+                    print("\t Printer connection established")
+                    setupPrinter(prtConn)
+                    prtReady = True
+    except Exception as e:
+        print("\n\t Exception detected: ", e)
 
-            while True:
-                ready = input("\n\t Ready for picture capture? y or n: ")
-                if "Y" == ready.upper():
-                    print("\n")
-                    break
-            # ready for the shots to begin
-            # move printer to start positioning and clear camera polling buffer
-            slowMove(prtConn, y= -round(bedMoveIncrement*3,1)) # start before subject
-            result = getLastEvent(r5Session) #clear polling buffer in camera
+    if not prtConn is None:
+        # display printer status
+        print("\tPrinter is connected: ", prtConn.is_open)
+        printBedPosition(prtConn)
+    else:
+        print(
+            "\t Printer is not connected. Ensure printer is on and cable is connected"
+        )
+        prtConn = None
+        prtReady = False
 
-            startTime = datetime.now()
-            for x in range(numShots):
-                slowMove(prtConn, y=bedMoveIncrement)
-                shootR5Image(session=r5Session, af=False)
+    input("Press ENTER key to return to Main Menu ...")
 
-            printBedPosition(prtConn) # final positon in Y-axis should be ~subject length
-            stopTime = datetime.now()
-            print("\n\t ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-            print("\t    shot sequence completed. Elapsed time = ", (stopTime-startTime))
-            paramTxt = "\n\tFStop= {fStop} Lens_length = {fLen}mm distance_to_object = {sDist}mm subject_size = {sLen}mm"
-            print(paramTxt.format(fStop=fStop, fLen=focalLen, sDist=subjectDist, sLen=subjectLen))
-            printShotEstimate(bedMoveIncrement, numShots) # tell user about time info for these shots
-            # report image file names that were captured
-            result = getLastEvent(r5Session)         # get all events from polling buffer
-            addedList = result.get("addedcontents")  # only care about image(s) added
-            print("\tImages captured:")
-            for image in range(len(addedList)):
-                print("\t\t",addedList[image])
-            print("\t\t total image count = ", len(addedList))
-            print("\n\t ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
-            # see if files should be copied
-            cf = input("\n\t Copy files from camera to local directory?  y or n: ")
-            if "Y" == cf.upper():
-                copyFiles(r5Session, addedList)
-            else:
-                print(" \t...Files requested not to be copied locally")
+def checkCameraStatus():
+    global r5Session, camReady
+    print("\tcheckCameraStatus")
+    if r5Session is None:
+        resp = input("\tCamera is not connected: Retry connection (y) or n: ")
+        if resp == "" or "Y" == resp.upper():
+            r5Session, camReady = createR5Session()
 
+    if not r5Session is None:
+        # display camera status
+        try:
+            reportBatteryStatus(r5Session)
+            print("\nCamera is connected")
+
+        except Exception as e:
+            print("\n\t Exception detected: ", e)
+            r5Session = None
+            camReady = False
+    else:
+        print(
+            "\t Camera is not connected. Ensure camera's CCAPI connection is set to Connect"
+        )
+
+    input("Press ENTER key to return to Main Menu ...")
+
+
+def defineShotParameters():
+    global dof, bedMoveIncrement, numShots
+
+    print("\n\t --- Define Shot Parameters ---")
+    while True:
+        try:
+            getShotParams()
+            dof = depthOfField(dist=subjectDist, fStop=fStop, focalLen=focalLen)
+            bedMoveIncrement, numShots = determineShotMovements(dof, subjectLen)
+
+            # tell user about time info for these shots
+            printShotEstimate( bedMoveIncrement, numShots )
+
+            ready = input("\n\t Happy with Shot Parameters? (y) or n: ")
+            if ready == "" or "Y" == ready.upper():
+                print("\n")
+                break
+        except Exception as ex:
+            print("-+" * 25)
+            print("-+-+   Exception occurred: ",ex)
+            print("-+-+   Please enter corrected shot parameters ")
+            print("-+" * 25)
+
+
+def checkShotEndpoints():
+    global shotDirection, prtConn, bedMoveIncrement, numShots
+    print("\n\t --- Check Shot Endpoints ---\n")
+    print("\t Allows photographer to check the lighting and composition of the")
+    print("\t subject at the begining and end of the shooting distances as")
+    print("\t specified by the Define Shot Parameters menu option. \n")
+
+    while True:
+        if shotDirection > 0:
+            print("\n\t Current shot direction is from Front to Back")
         else:
-            print("-" * 45)
-            print("\t Interface connection error detected:")
-            print("\t\t 3D Printer ready: ",prtReady)
-            print("\t\t R5 camera ready : ",camReady)
-            print("-" * 45)
-            break   # fatal error, exit the loop and exit
-        # query if we want to setup for another shoot
-        ready = input("\n\t Reset printer bed for another series of shots? y or n: ")
-        if "N" == ready.upper():
+            print("\n\t Current shot direction is from Back to Front")
+
+        tempStr = input("\t Is current shot direction correct? (y) or n: ")
+        if tempStr == "" or "Y" == tempStr.upper():
             print("\n")
             break
+
+        shotDirection *= -1 # change direction and prompt again to verify
+
+    # cycle printer bed through endpoints until everyone is happy
+    setRelPositioning(prtConn)  # 91
+    positionLabel = ["Unkown", "Front", "Back"]
+    directionTxt = "\t Current position: {}    "
+    endptLocation = shotDirection
+    while True:
+        print(directionTxt.format(positionLabel[endptLocation]))
+        printBedPosition( prtConn )
+        tempStr = input("\tPress Enter to continue, or any key to exit endpoint checks and go back to home ")
+        if not tempStr == "":
+            # head back to shot's starting position
+            slowMove(prtConn, y=0) # origin was already set during printer setup
+            break
+
+        # compute and head to next endpoint location
+        ypos = round(bedMoveIncrement * numShots * endptLocation, 2)
+        slowMove(prtConn, y=ypos)
+        endptLocation *= -1
+
+
+def performShotCaptures():
+    global shotDirection, prtConn, r5Session,bedMoveIncrement, numShots
+    print("\n\t --- Perform Shot Captures ---\n")
+    if prtReady and camReady:
+        # move printer to start positioning and clear camera polling buffer
+         # start before subject
+#        slowMove(prtConn, y=-round(bedMoveIncrement * 3 * shotDirection, 2))
+        slowMove(prtConn, y=0)
+        setRelPositioning(prtConn)  # 91
+        result = getLastEvent(r5Session)  # clear polling buffer in camera
+
+        startTime = datetime.now()
+        for x in range(numShots):
+            slowMove(prtConn, y=bedMoveIncrement * shotDirection)
+            shootR5Image(session=r5Session, af=False)
+
+        # final positon in Y-axis should be ~subject length
+        printBedPosition( prtConn )
+        stopTime = datetime.now()
+        print("\n\t ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        print("\t    shot sequence completed. Elapsed time = ", (stopTime - startTime))
+        paramTxt = "\n\tFStop= {fStop} Lens_length = {fLen}mm distance_to_object = {sDist}mm subject_size = {sLen}mm"
+        print(paramTxt.format(fStop=fStop, fLen=focalLen, sDist=subjectDist, sLen=subjectLen))
+        # tell user about time info for these shots
+        printShotEstimate( bedMoveIncrement, numShots )
+
+        # report image file names that were captured
+        result = getLastEvent(r5Session)  # get all events from polling buffer
+        addedList = result.get("addedcontents")  # only care about image(s) added
+        print("\tImages captured:")
+        for image in range(len(addedList)):
+            print("\t\t", addedList[image])
+        print("\t\t total image count = ", len(addedList))
+        print("\n\t ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+        # see if files should be copied
+        cf = input("\n\t Copy files from camera to local directory?  (y) or n: ")
+        if cf == "" or "Y" == cf.upper():
+            copyFiles(r5Session, addedList)
         else:
-            setAbsPositioning(prtConn)
-            gotoOrigin(prtConn)
-            setRelPositioning(prtConn)
-            # go back to entering shot parameters
+            print(" \t...Files requested not to be copied locally")
+    else:
+        # Printer or Camera connectivity not established
+        print("\n\t Error detected with connectivity as follows:")
+        print("\t Printer connectivity established: ", prtReady)
+        print("\t Camera connectivity established:  ", camReady)
+
+    input("Press ENTER key to return to Main Menu ...")
+
+def printBedLocation():
+    # show if printer is connected and current X, Y,Z coordinates
+    global prtConn
+    print("\tPrint Bed Location")
+    if prtConn is None:
+        print("\tPrinter is not connected... ")
+    else:
+        # display printer status
+        print("\tPrinter is connected: ", prtConn.is_open)
+        printBedPosition(prtConn)
+
+    input("Press ENTER key to return to Main Menu ...")
+
+def changeZAxis():
+    global prtConn
+    moveAxisZ(prtConn)
 
 
-except Exception as e:
+
+# main
+menuOptionDict = {
+    1: "Printer Status",
+    2: "Camera Status",
+    3: "Define Shot Parameters",
+    4: "Check Shot Endpoints",
+    5: "Perform Shot Captures",
+    6: "Print Bed Location",
+    7: "Change Z-axis",
+    8: "Exit",
+}
+
+try:
+    print("\n Starting to connect to printer and camera...\n")
+
+    while True:
+        printMenu()
+        selOption = ""
+        try:
+            selOption = int(input("Enter menu option: "))
+        except:
+            print("Wrong input choice selected. Please enter a valid number ...")
+
+        if selOption == 1:
+            checkPrinterStatus()
+        elif selOption == 2:
+            checkCameraStatus()
+        elif selOption == 3:
+            defineShotParameters()
+        elif selOption == 4:
+            checkShotEndpoints()
+        elif selOption == 5:
+            performShotCaptures()
+        elif selOption == 6:
+            printBedLocation()
+        elif selOption == 7:
+            changeZAxis()
+        elif selOption == 8:
+            print("\n\t Exiting program ...\n")
+            sys.exit()
+        else:
+            print(
+                "\n\tInvalid option selected. Enter number between 1 and ",
+                len(menuOptionDict),
+            )
+
+
+except Exception as caughtEx:
     print("-+" * 20)
     print("Fatal exception detected")
-    print(e)
+    print(caughtEx)
     print("-+" * 20)
     print("\n")
